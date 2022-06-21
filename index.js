f4a198d1eaa25766e4cb74a8d39d2b552adf17fe
Emmanuel Ojo oluwatobiloba <68256323+DeeMerry@users.noreply.github.com>
2022-06-21 15:42:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let SurName = "Ojo";
+let FirstName = "Emmanuel";
+let MiddleName = "Oluwatobiloba";
+let Height = "5.68cm";
+let Country = "Nigeria";
+
+
+Name = FirstName + ' ' + SurName + ' ' + MiddleName;
+
+document.write('Name: ' + ' ' + Name + '<br/>');
+document.write('Height: ' + Height + '<br/>');
+document.write('Country: ' + '' + Country );
